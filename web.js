844670958650962ede74a6916cf8faf87de6401f
Angelo Barrozo <barrozange@wonderland.ecs.vuw.ac.nz>
2014-05-20 23:07:46 +1200

    some changes

--- web.js
+++ web.js
@@ -8,7 +8,13 @@ var express = require('express')
 , client;
 
 var coords = [
-{ lat: 0, lon: 0 }
+{ lat: -42.5667, lon: 32.767 }, 
+{ lat: 35.76556, lon: 75.5675 }, 
+{ lat: -35.65756, lon: 20.656 }, 
+{ lat: 67.356756, lon: 45.896 }, 
+{ lat: -50.567, lon:185.3450 }, 
+{ lat: 0, lon: 0 }, 
+{ lat: -40.930, lon: 173.050 }
 ]
 
 // make express handle JSON and other requests
@@ -17,6 +23,11 @@ app.use(express.bodyParser());
 app.use(express.static(__dirname));
 // if not able to serve up a static file try and handle as REST invocation
 app.use(app.router);
+
+
+app.get("/location", function (req, res) {
+	res.send(coords);
+});
 
 //do location stuff
 app.post('/location', function(req, res) {
@@ -27,17 +38,25 @@ app.post('/location', function(req, res) {
 	}
 
 	var newCoords = {
-		author : req.body.author,
-		text : req.body.text
+		lat : req.body.lat,
+		lon : req.body.lon
 	};
 
 	coords.push(newCoords);
   // should send back the location at this point
   console.log("Added!");
-  newCoords.pos = coords.length-1;
+  //newCoords.pos = coords.length-1;
+  res.json(true);
   res.send(newCoords);
 
 });
+
+app.get('/location/random', function(req, res) {
+  var id = Math.floor(Math.random() * coords.length);
+  var q = coords[id];
+  res.send(q);
+});
+
 
 //get user id...
 app.get('/user:id', function(req, res) {
@@ -51,7 +70,6 @@ app.post("/gallery", function(req, res) {
 });
 
 
-// use PORT set as an environment variable
-var server = app.listen(process.env.PORT, function() {
+var server = app.listen(3000, function () {
 	console.log('Listening on port %d', server.address().port);
 });
