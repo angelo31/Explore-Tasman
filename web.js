fd1c0e097f161e3dd36ba13adfe63a957fe120f8
Angelo Barrozo <barrozange@wonderland.ecs.vuw.ac.nz>
2014-05-20 21:17:34 +1200

    updates

--- web.js
+++ web.js
@@ -1,11 +1,15 @@
 var express = require('express')
 // , bodyParser = require('body-parser')
 , app = express()
-// , pg = require('pg').native
-// , connectionString = process.env.DATABASE_URL
+, pg = require('pg').native
+, connectionString = process.env.DATABASE_URL
 // , start = new Date()
 // , port = process.env.PORT
-// , client;
+, client;
+
+var coords = [
+{ lat: 0, lon: 0 }
+]
 
 // make express handle JSON and other requests
 app.use(express.bodyParser());
@@ -14,12 +18,40 @@ app.use(express.static(__dirname));
 // if not able to serve up a static file try and handle as REST invocation
 app.use(app.router);
 
-
+//do location stuff
 app.post('/location', function(req, res) {
-  res.send("Hello World")
+	console.log(req.body);
+	if(!req.body.hasOwnProperty('lat') || !req.body.hasOwnProperty('lon')) {
+		res.statusCode = 400;
+		return res.send('Error 400: Post syntax incorrect.');
+	}
+
+	var newCoords = {
+		author : req.body.author,
+		text : req.body.text
+	};
+
+	coords.push(newCoords);
+  // should send back the location at this point
+  console.log("Added!");
+  newCoords.pos = coords.length-1;
+  res.send(newCoords);
+
 });
+
+//get user id...
+app.get('/user:id', function(req, res) {
+	res.send("Hello World")
+});
+
+//get photos and post in gallery
+app.post("/gallery", function(req, res) {
+
+
+});
+
 
 // use PORT set as an environment variable
 var server = app.listen(process.env.PORT, function() {
-    console.log('Listening on port %d', server.address().port);
+	console.log('Listening on port %d', server.address().port);
 });
