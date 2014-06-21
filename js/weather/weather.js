f9384ce9cfde0ed3926b250561c9867724f7edb9
MIkey <mikey@mikeyPC.(none)>
2014-06-22 00:49:17 +1200

    final merge....

--- js/weather/weather.js
+++ js/weather/weather.js
@@ -10,11 +10,11 @@ function getWeather() {
     zipcode: '',
 	unit: 'c',
     success: function(weather) {
-      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-      html += '<li class="currently">'+weather.currently+'</li>';
+//      html = '<h2><i class="icon-'+weather.code+'"></i>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+        html = '<ul><li>'+weather.city+weather.region+'</li>';
+//      html += '<li class="currently">'+weather.currently+'</li>';
 //      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
-      html += '<p>'+weather.updated+'</p>';
+//      html += '<p>'+weather.updated+'</p>';
   
   for(var i=0;i<weather.forecast.length;i++) {
         html += '<li class="forcast">'+weather.forecast[i].day+': '+weather.forecast[i].high+'</li>';
